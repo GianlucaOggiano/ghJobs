8706455ee668d342fe20414dbc2d0262e0744942
Gianluca Oggiano <gianlucao89@gmail.com>
2021-02-23 11:02:37 +0100

    Job detail page

---
+++ src/pages/Job.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import JobDetail from '../components/JobDetail';
+import JobSidebar from '../components/JobSidebar';
+import { fetchJob } from '../redux/actions';
+
+const Job = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { selectedJob, loading } = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(fetchJob(id));
+  }, [dispatch, id]);
+
+  return (
+    <div>
+      <main>
+        {selectedJob && (
+          <>
+            <JobSidebar apply={selectedJob.how_to_apply} />
+            <JobDetail job={selectedJob} />
+          </>
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default Job;
