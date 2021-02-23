d3050ae5ff1c504e90db10536a47935d4d6714c5
Gianluca Oggiano <gianlucao89@gmail.com>
2021-02-23 11:14:15 +0100

    clean

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { SET_FULL_TIME } from '../../redux/types';
 import SidebarLocation from '../SidebarLocation';
